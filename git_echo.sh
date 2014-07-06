91a85e6a5b40a7b91462ca58649e95069d8f10fb
sinsnk <cb.so.devope@gmail.com>
2014-07-06 14:39:22 +0900

    shell modify

--- git_echo.sh
+++ git_echo.sh
@@ -4,19 +4,21 @@ SIN="sinsnk"
 YUI="yuinchirn"
 
 if [ $# = 4 ]; then
+echo "$# 引数正常"
 	#add->commit->push
-	git add $1
-	git commit -m $2
-	git push origin $3
+	git add "$2"
+	git commit -m "$3"
+	git push origin "$4"
 
-	if [ $0 = $SIN ]; then
-		echo $SIN
+	if [ "$1" = "$SIN" ]; then
+		echo "$0 引数正常"
+		echo "$SIN"
 		git rebase -i --root
 		git commit --amend --author="sinsnk <cb.so.devope@gmail.com>"
 		git rebase --continue
 		git push -f
-	elif [ $0 = $YUI ]; then
-		echo $YUI
+	elif [ "$1" = "$YUI" ]; then
+		echo "$YUI"
 		git rebase -i --root
 		git commit --amend --author="yuinchirn <l.serah.yuinch@gmail.com>"
 		git rebase --continue
