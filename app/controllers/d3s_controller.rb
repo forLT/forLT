701b2ec8d712b603825353a3ed108832e2d68e6e
yuinchirn-dev <yuta.chiba0301@gmail.com>
2014-07-28 11:00:52 +0000

    modified

--- app/controllers/d3s_controller.rb
+++ app/controllers/d3s_controller.rb
@@ -2,11 +2,20 @@ class D3sController < ApplicationController
   def index
     @title = "D3 デモ"
     @name = "テスト"
+    @player1 = "sasaki"
+    @player2 = "ogasawara"
+    @player3 = "chiba"
   end
   
   def create
   end
   
-  def new 
+  def new
+    @title = "skybox"
   end
+  
+  def new2
+    @title = "skybox"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'projects#index'
+  
+  get 'd3s/new2' => 'd3s#new2'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
